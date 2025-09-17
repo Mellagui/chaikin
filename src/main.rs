cc1575e3eb07dd57d499e4d0d86c0b0bde7ac748
AKMHZJ <akmhzj@gmail.com>
2025-09-17 13:16:23 +0100

    add the message bonus

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ use macroquad::prelude::*;
 mod chaikin;
 use chaikin::*;
 
-#[macroquad::main("CHAIKIN")]
+#[macroquad::main("CHAIKIN => Left Click -> Add Point | Enter -> Animate | Esc -> Quit | c -> restar")]
 async fn main() {
     let mut points: Vec<(f32, f32)> = Vec::new();
     let mut tmp_points: Vec<(f32, f32)> = Vec::new();
     let mut started = false;
+    let mut message = false;
     let mut step = 0;
     let mut timer = get_time();
 
@@ -21,19 +22,33 @@ async fn main() {
             started = false;
             points = Vec::new();
             step = 0;
+            message = false;
         }
-        if is_key_pressed(KeyCode::Enter) && points.len() > 1 {
-            started = true
+        if is_key_pressed(KeyCode::Enter)  {
+            if points.is_empty() {
+                message = true;
+            } else {
+                started = true;
+            }
         }
         if is_mouse_button_pressed(MouseButton::Left) && !started {
             let (x, y) = mouse_position();
             points.push(( x, y ));
+            message = false;    
         }
         for point in points.iter() {
-            draw_circle_lines(point.0, point.1, 5.0, 1. , RED);
+            draw_circle_lines(point.0, point.1, 3.0, 1. , RED);
         }
 
-        if started {
+        if message {
+            // draw_text("click on the mouse to draw the point!!!", 20.0, 20.0, 30.0, WHITE);
+            let text = "click on the mouse to draw the point!!!";
+            let font_size = 30.0;
+            let text_dims = measure_text(text, None, font_size as u16, 1.0);
+            let x = (screen_width() - text_dims.width) / 2.0;
+            let y = screen_height() / 2.0;
+            draw_text(text, x, y, font_size, WHITE);
+        } else if started && points.len() > 1 {
             if step < 7 && get_time() > timer + 0.5 {
                 tmp_points = chaikin(&points, step);
                 step += 1;
@@ -49,10 +64,9 @@ async fn main() {
             for i in 0..tmp_points.len() - 1 {
                 let p1 = tmp_points[i];
                 let p2 = tmp_points[i + 1];
-                draw_line(p1.0, p1.1, p2.0, p2.1, 2.0, YELLOW);
+                draw_line(p1.0, p1.1, p2.0, p2.1, 1.0, GREEN);
             }
         }
-
         next_frame().await;
     }
 }
